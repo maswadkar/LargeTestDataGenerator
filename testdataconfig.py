40ee32b36e1433d90898f674b83f8cbe10ba5cc4
maswadkar <maswadkar@gmail.com>
2020-02-11 08:51:52 +0000

    created gitignore file

---
+++ testdataconfig.py
@@ -0,0 +1,7 @@
+#this is just a Credential File
+mongoCredentials = dict(user='scott', 
+                        password='tiger',
+                        server='twitterx.organic-farmer.in',
+			port='27017',
+			connectionstring = 'mongodb://scott:tiger@twitterx.organic-farmer.in:27017')
+
